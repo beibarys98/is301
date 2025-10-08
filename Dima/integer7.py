3bb4242cce25f101613938f32d891a2b2c175f38
Your Name <your.email@example.com>
2025-10-08 14:48:44 +0500

    Add scripts for integer input and digit manipulation

---
+++ Dima/integer7.py
@@ -0,0 +1,8 @@
+num = int(input("Введите двузначное число: "))
+tens = num // 10
+units = num % 10
+sum_digits = tens + units
+product_digits = tens * units
+print("Сумма цифр:", sum_digits)
+print("Произведение цифр:", product_digits)
+
